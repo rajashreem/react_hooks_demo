d70d307543a26cf08f39c3fdbc04a231cb4a962a
Rajashree <rajashreermalvade@gmail.com>
2019-07-19 15:39:22 +0530

    Add name field to hooks example

--- src/StateWithEffect.js
+++ src/StateWithEffect.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function StateWithEffect() {
     const [count, setCount] = useState(0); 
+    const [name, setName] = useState(''); 
 
     useEffect(() => {
       document.title = `${count} times`;
@@ -9,9 +10,15 @@ function StateWithEffect() {
 
     return(
       <div>
-        <p>You clicked {count} times</p>
-        <button onClick={() => {setCount(count+1)}}>Click me to increase count</button>
-        <button onClick={() => {setCount(count-1)}}>Click me to decrease count</button>
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => {setCount(count+1)}}>Click me to increase count</button>
+          <button onClick={() => {setCount(count-1)}}>Click me to decrease count</button>
+        </div>
+        <div className='form'>
+          <input value={name} placeholder="Enter your name here" onChange={(event) => setName(event.target.value)}></input>
+          <p>Welcome {name} </p>
+        </div>
       </div>
     );
 }
