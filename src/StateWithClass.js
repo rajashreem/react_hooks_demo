4a75eef53ea5415a5355992315c6f5821da75f26
Rajashree <rajashreermalvade@gmail.com>
2019-07-19 15:35:06 +0530

    Add name field to class example

--- src/StateWithClass.js
+++ src/StateWithClass.js
@@ -4,7 +4,8 @@ class StateWithClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0, 
+      name: ''
     }
   }
 
@@ -16,16 +17,25 @@ class StateWithClass extends React.Component {
     document.title = `${this.state.count} times`;
   }
 
+  handleNameChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
   render() {
     return(
       <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={() => {this.setState({count : this.state.count + 1})}}>Click me to increase count</button>
-        <button onClick={() => {this.setState({count : this.state.count - 1})}}>Click me to decrease count</button>
+        <div>
+          <p>You clicked {this.state.count} times</p>
+          <button onClick={() => {this.setState({count : this.state.count + 1})}}>Click me to increase count</button>
+          <button onClick={() => {this.setState({count : this.state.count - 1})}}>Click me to decrease count</button>
+        </div>
+        <div className='form'>
+          <input value={this.state.name} placeholder="Enter your name here" onChange={this.handleNameChange}></input>
+          <p>Welcome {this.state.name} </p>
+        </div>
       </div>
     );
   }
-
 }
   
 export default StateWithClass;
